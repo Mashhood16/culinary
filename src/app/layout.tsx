c1e477b60b691472cc725b3fc515546c0e222bc7
Mashhood16 <thexssrodent@gmail.com>
2026-06-19 04:44:38 +0500

    responsive

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       suppressHydrationWarning
     >
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <script
           dangerouslySetInnerHTML={{
             __html: `
